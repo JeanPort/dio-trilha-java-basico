ce4498c768395b53fab4be79e29aed1fe8caf6b9
Jean Port <pierreanatc522@gmail.com>
2022-12-06 00:35:13 -0300

    Initial commit

---
+++ src/com/jean/variaveis/Numero.java
@@ -0,0 +1,7 @@
+package com.jean.variaveis;
+
+public class Numero {
+    public static void main(String [] args) {
+
+    }
+}
---
+++ src/com/jean/variaveis/Variaveis.java
@@ -0,0 +1,7 @@
+package com.jean.variaveis;
+
+public class Variaveis {
+    public static void main(String [] args){
+
+    }
+}
